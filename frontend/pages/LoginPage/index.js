025d828406a6274c65190886ec851d6b9bf78063
unknown <trinhcamminh25112002@gmail.com>
2022-10-04 21:15:59 +0700

    refactor code

--- frontend/pages/LoginPage/index.js
+++ frontend/pages/LoginPage/index.js
@@ -1,16 +1,82 @@
-import "./index.css";
-export var LoginPage = function LoginPage() {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "div",
-      { className: "background" },
-      React.createElement(
-        "h1",
-        { className: "text-3xl font-bold underline" },
-        "Login Page"
-      )
-    )
-  );
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const LoginPage = () => {
+    return (
+        <>
+            <div className='background lg:h-full h-screen bg-gradient-to-r from-yellow-300 to-green-400'>
+                <div className=' px-2 py-6 my-0 mx-auto w-11/12 rounded-b-lg bg-white lg:w-2/12 md:w-4/12 sm:w-full shadow-lg'>
+                    <h1 className='text-4xl  font-bold text-center'>
+                        <a href='/' className='text-green-600'>
+                            i
+                        </a>
+                        Banking
+                    </h1>
+                </div>
+                <form className='login-form my-0 mx-auto lg:mb-56 xl:mt-64 xl:mb-64 mt-12 mb-6 w-11/12 sm:mt-64 sm:mb-40 md:mb-72 lg:mt-20 md:mt-64 sm:w-11/12 md:w-6/12 lg:w-3/12 shadow-lg p-5 ml-6/12 bg-white rounded-lg'>
+                    <label className='font-bold text-4xl mb-3 text-green-500'>Log In</label>
+                    <label for='username' className='block mt-3 text-gray-700 text-md font-bold mb-1'>
+                        Username
+                    </label>
+                    <input
+                        required
+                        placeholder='your username'
+                        type='text'
+                        className='peer shadow mb-3 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline focus:border-green-400'
+                    />
+                    <label for='password' className='block text-gray-700 text-md font-bold mb-1'>
+                        Password
+                    </label>
+                    <input
+                        required
+                        placeholder='your password'
+                        type='text'
+                        className='peer shadow mb-1 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline focus:border-green-400'
+                    />
+                    <div
+                        class='peer-invalid:visible flex bg-red-100 rounded-lg p-3 mb-4 mt-4 text-md text-red-700'
+                        role='alert'
+                    >
+                        <svg
+                            class='w-5 h-5 inline mr-3'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                fill-rule='evenodd'
+                                d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+                                clip-rule='evenodd'
+                            ></path>
+                        </svg>
+                        <div>
+                            <span class='font-medium'>Error!</span> Username or password invalid
+                        </div>
+                    </div>
+                    <p>
+                        <input required type='checkbox' class='rounded text-blue-500' /> I accept with{' '}
+                        <a href='/' className='font-bold italic text-green-500'>
+                            term of uses
+                        </a>
+                        <a href='/'> and </a>
+                        <a href='/' className='font-bold italic text-green-500'>
+                            policy
+                        </a>
+                    </p>
+
+                    <br />
+                    <Link to='/transaction'>
+                        <button
+                            type='submit'
+                            className='bg-green-600 hover:bg-green-400 w-full px-6 p-2 rounded-md text-white font-bold'
+                        >
+                            {' '}
+                            Log In{' '}
+                        </button>
+                    </Link>
+                </form>
+                <p className='text-green-500 italic text-center font-bold'> &copy; DMT Team</p>
+            </div>
+        </>
+    );
 };
