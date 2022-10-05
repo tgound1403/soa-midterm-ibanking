332b4a37f37f28369de392988b0c8a1b039076dd
TrinhCamMinh <trinhcamminh25112002@gmail.com>
2022-10-05 16:25:36 +0700

    update signup controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,18 @@
-const loginUser = (req, res) => {
-    res.json({ message: 'login page' });
+const User = require('../models/userModel');
+
+const signupUser = async (req, res) => {
+    const { additionalName, password, email, telephone, balance, amount } = req.body;
+
+    try {
+        const user = await User.signup(additionalName, password, email, telephone, balance, amount);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
-const signupUser = (req, res) => {
-    res.json({ message: 'signup page' });
+const loginUser = (req, res) => {
+    res.json({ message: 'login page' });
 };
 
 module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const userSchema = new Schema(
     {
         additionalName: {
             type: String,
             required: true,
+            unique: true,
         },
         password: {
             type: String,
@@ -17,7 +20,7 @@ const userSchema = new Schema(
             unique: true,
         },
         telephone: {
-            type: Number,
+            type: String,
             required: true,
             unique: true,
         },
@@ -32,5 +35,32 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 );
+
+userSchema.statics.signup = async (additionalName, password, email, telephone, balance, amount) => {
+    const exist = mongoose.findOne({ additionalName });
+    if (!additionalName || !password || !email || !telephone || !telephone || !!balance || !amount) {
+        throw Error('All fill must be filled');
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid');
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough');
+    }
+
+    //check if username is existed or not
+    if (exist) {
+        throw Error('username already in use');
+    }
+
+    //encrypted password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await userSchema.create({ additionalName, password: hash, email, telephone, balance, amount });
+    return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
