d3041b2d09437dcfb85d0e5a0d5feac21cdd3d3f
TrinhCamMinh <trinhcamminh25112002@gmail.com>
2022-10-13 20:57:05 +0700

    delete redundant comment

--- frontend/src/hooks/useDebounce.jsx
+++ frontend/src/hooks/useDebounce.jsx
@@ -3,6 +3,7 @@ import { useTimeOut } from './useTimeOut';
 export const useDebounce = (callback, delay, dependencies) => {
     const { reset, clear } = useTimeOut(callback, delay);
     useEffect(reset, [...dependencies, reset]);
+
     //this seconde useEffect is to prevent the useTimeOut function run when component is
     //mounted for the first time
     useEffect(clear, [clear]);
--- frontend/src/hooks/useTimeOut.jsx
+++ frontend/src/hooks/useTimeOut.jsx
@@ -1,40 +1,32 @@
 import { useEffect, useCallback, useRef } from 'react';
 
 export const useTimeOut = (callback, delay) => {
-    const callbackRef = useRef();
+    const callbackRef = useRef(callback);
     const timeoutRef = useRef();
 
-    //if callback parameter really change then we redefine the callback function
-    //otherwise we keep it the same with the old callback function
-    //note that this old callback function still work as usual expect React will not re-render this
-    //callback function again (refer references type in JS)
     useEffect(() => {
         callbackRef.current = callback;
     }, [callback]);
 
-    //if delay parameter is changed then redefine this setTimeOut function
-    //otherwise we keep it the same
     const set = useCallback(() => {
         timeoutRef.current = setTimeout(() => {
             callbackRef.current();
         }, delay);
     }, [delay]);
 
-    //this callback function will only defined one time so the next time
-    //React won't re-render this callback function anymore
     const clear = useCallback(() => {
         timeoutRef.current && clearTimeout(timeoutRef.current);
     }, []);
 
-    //if one those three changes, the set() function run
-    //then clear it, note that set and clear is changed when the dependency
-    // of these function change (because when those function redefined then JS will consider them as the new one)
+    //note that cleanup function alway be called before the callback
+    //Ex: At first, the callback will be execute then return cleanup function
+    //Next time, because we return cleanup function so the next time clean up function
+    //will be called first then callback
     useEffect(() => {
         set();
         return clear;
     }, [delay, set, clear]);
 
-    //this only reset the set() to make it run again
     const reset = useCallback(() => {
         clear();
         set();
