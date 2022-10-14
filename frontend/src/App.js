552d0c8163e46169e9f35e0710dbf64f3e51aa66
TrinhCamMinh <trinhcamminh25112002@gmail.com>
2022-10-14 09:56:26 +0700

    update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import "react-app-polyfill/stable";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./pages/LoginPage/index.js";
-import { TransactionForm } from "./pages/TransactionPage";
-import React from "react";
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<LoginPage />} />
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/transaction" element={<TransactionForm />} />
-      </Routes>
-    </BrowserRouter>
-  );
-=======
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from './pages/login';
@@ -32,5 +14,4 @@ export default function App() {
             </Routes>
         </BrowserRouter>
     );
->>>>>>> MinhBranch
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-// import reportWebVitals from './reportWebVitals';
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-=======
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -26,4 +10,3 @@ root.render(
         <App />
     </AuthProvider>
 );
->>>>>>> MinhBranch
